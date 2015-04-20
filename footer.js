15c02a5d1a90cc1f7f25d879263ff3936ed43d97
Jason Chiu <jforeza@gmail.com>
2015-04-20 09:20:08 -0700

    Tentative website updates, planning to do more later today. SSI doesn't work on github. Sad face.

--- footer.js
+++ footer.js
@@ -3,7 +3,7 @@
   var foot = { footerText1:  "I'm a 3rd year student at the University of California, Irvine, and expect to graduate in 2016." 
   			, footerText2: "I've worked on numerous game projects. I currently serve as Vice President of the Video Game Development Club (VGDC) at UCI."
   			, footerText3: "I've picked up web development through participating in hackathons and app jams; and made this website to showcase my work."
-  			, footerText4: "Tools for this site include Materialize, github for hosting, HTML/CSS/Javascript as languages, and Angular.js. © 2014 Design by Jason Chiu"
+  			, footerText4: "Tools for this site include Materialize, github for hosting, HTML/CSS/Javascript as languages, and Angular.js. © 2015 Design by Jason Chiu"
   			, name1: "Linkedin"
   			, name2: "Github"
   			, name3: "Medium"
