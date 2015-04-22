78843331829a1f7ed1ba586e1b27072eee829b6b
Jason Chiu <jforeza@gmail.com>
2015-04-22 09:09:04 -0700

    Updated index again

--- footer.js
+++ footer.js
@@ -1,9 +1,9 @@
 (function(){
   // Footer
   var foot = { footerText1:  "I'm a 3rd year student at the University of California, Irvine, and expect to graduate in 2016." 
-  			, footerText2: "I've worked on numerous game projects. I currently serve as Vice President of the Video Game Development Club (VGDC) at UCI."
-  			, footerText3: "I've picked up web development through participating in hackathons and app jams; and made this website to showcase my work."
-  			, footerText4: "Tools for this site include Materialize, github for hosting, HTML/CSS/Javascript as languages, and Angular.js. © 2015 Design by Jason Chiu"
+  			, footerText2: "Tools for this site include Materialize, github for hosting, HTML/CSS/Javascript as languages, and Angular.js. © 2015 Design by Jason Chiu"
+  			, footerText3: ""
+  			, footerText4: ""
   			, name1: "Linkedin"
   			, name2: "Github"
   			, name3: "Medium"
